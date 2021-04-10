181f32f2be3a877c2677d06673b5df15eb3bda9d
imabiodun <70638812+imabiodun@users.noreply.github.com>
2021-04-10 10:17:05 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+/*navLink=document.getElementById("home")
+
+const goto= function(navLink,landhere){
+    return document.getElementById(landhere);
+
+}
+getLink=document.getElementsByClassName("nav-bar");
+
+var a=document.createElement("a");
+enterHTML.appendChild("link");*/
